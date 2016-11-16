5d2f2ece6e152f6835e16be38258ccc4658423d8
vagrant <vagrant@vagrant.vm>
2016-11-16 01:59:30 +0000

    False alarm on the last commit. Creates nearly the dictionary needed for time_data.json, now just working on creating something of a set out of the coordinates dictionaries

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ from model import db, connect_to_db, Species, Event, Animal
 
 from datetime import datetime
 
+from itertools import chain
+
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug DebugToolbarExtension
@@ -88,39 +90,81 @@ def get_animal_data():
 def get_time_data():
 
     #get all the animal ids
-    animal_ids = db.session.query(Animal.animal_id).all()
-    print animal_ids
+    # animal_ids = db.session.query(Animal.animal_id).all()
+    # print animal_ids
     
-    # animal_positions = {}
-    for animal_id in animal_ids:
-        animal = animal_id[0]
-        events = db.session.query(Event.timestamp).filter(Event.animal_id==animal).limit(500).all()
+    # coordinates = {}
+    # for animal_id in animal_ids:
+    #     animal = animal_id[0]
+    #     events = db.session.query(Event.timestamp).filter(Event.animal_id==animal).limit(500).all()
 
-        animal_positions = {}
-        # import pdb
-        # pdb.set_trace()
-        # date_list = []
-        for event in events:
-            month = event[0].month
-            day = event[0].day
-            year = event[0].year
-            date = str(month) + " " + str(day) + ", " + str(year)
+    #     animal_positions = {}
+    #     import pdb
+    #     pdb.set_trace()
+        # for event in events:
+        #     month = event[0].month
+        #     day = event[0].day
+        #     year = event[0].year
+            # date = str(month) + " " + str(day) + ", " + str(year)
             #query database for animal coordinate at this date
-            coordinate = db.session.query(Event.long_location, Event.lat_location).filter(month==extract('month', Event.timestamp), day==extract('day', Event.timestamp), Event.animal_id==animal).first()
+            # coordinate = db.session.query(Event.long_location, Event.lat_location).filter(month==extract('month', Event.timestamp), day==extract('day', Event.timestamp), Event.animal_id==animal).first()
             #put coordinate in its own list
-            animal_coordinate = [coordinate[0], coordinate[1]]
-            #break this into if else for clarity    
-            if date not in animal_positions:
-                animal_positions[date] = [animal, animal_coordinate]
-            else:
-                animal_positions[date].append([animal, animal_coordinate])
+            # animal_coordinate = [coordinate[0], coordinate[1]]  
+            # if date not in animal_positions:
+            #     animal_positions[date] = [[animal, animal_coordinate]]
+            # else:
+            #     animal_positions[date].append([animal, animal_coordinate])
             # event_dict[animal] = event_dict.get(animal, animal_coordinate)
             # date_list.append(date)
 
         # for date in date_list:
-        # animal_positions[str(date)] = animal_positions.get(date, event_dict)
+        # coordinates["coordinates"] = animal_positions
             #need to be able to append multiple animal coordinates to a date
 
+    #get all events
+
+    events = db.session.query(Event.timestamp).filter(extract('year', Event.timestamp)==2016, extract('month', Event.timestamp)==04, extract('day', Event.timestamp)==30).all()
+
+    # import pdb
+    # pdb.set_trace()
+
+    animal_positions = {}
+    #for each day get all distinct animals
+    for event in events:
+        month = event[0].month
+        day = event[0].day
+        year = event[0].year
+        date = str(month) + "/" + str(day) + "/" + str(year)
+        animal_ids = db.session.query(Event.animal_id).filter(month==extract('month', Event.timestamp), day==extract('day', Event.timestamp)).distinct()
+        
+        coordinates = {}
+        #for each event's distinct animal get first coordinate
+        for animal_id in animal_ids:
+            animal = animal_id[0]
+            coordinate = db.session.query(Event.long_location, Event.lat_location).filter(month==extract('month', Event.timestamp), day==extract('day', Event.timestamp), Event.animal_id==animal).first()
+            animal_coordinate = [coordinate[0], coordinate[1]]
+            # if animal not in coordinates:
+            coordinates[animal] = [animal_coordinate]
+            # else:
+                # coordinates
+
+        date_string = str(date)
+
+        coordinates_update = {}
+        for coordinate in coordinates:
+            coordinates_update.update(coordinate)
+        # coordinates = dict(chain(coordinates))
+        # coordinate_set = set(coordinates)
+
+        # coordinates = dict(coordinate_set)
+
+        import pdb
+        pdb.set_trace()
+
+        # if date_string not in animal_positions:
+        animal_positions[date_string] = coordinates_update
+        # else:
+        #     animal_positions[date_string].append(coordinates)
             # if event in animal:
                 # print "hi",type(animal)
                 #put the coordinates in an animal coordinates variable
